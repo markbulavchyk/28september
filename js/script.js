f24e1b572edd8cc5b8a6149a29f32b58f9a537f3
Mark <example@gmail.com>
2023-09-28 17:44:36 -0600

    practice ,repeat, lesson19

--- js/script.js
+++ js/script.js
@@ -1,10 +1,22 @@
 'use strict';
 
-const a = {
-    objectArr: [],
-    obj: {
-        is: true
-    }
-};
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
 
-console.log(a);
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+}
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+

0294b6ce799796b552c836917df88db2ed60b5eb
Mark <example@gmail.com>
2023-10-09 13:38:34 -0600

    lesson 31 , callback

--- js/script.js
+++ js/script.js
@@ -1,56 +1,27 @@
 'use strict';
 
-// first: for (let i = 0; i < 3; i++) {
-//     console.log(`First level: ${i}`);
+function first() {
 
-//     for (let j = 0; j < 3; j++) {
-//         console.log(`Second level: ${j}`);
-
-//         for (let k = 0; k < 3; k++) {
-//             if (k === 2) continue first
-//             console.log(`Third level: ${k}`);
-//         }
-//     }
-// }
-
-// function getMathResult(base, repeat) {
-//     let str = '';
-
-//     if (typeof repeat != 'number' || repeat <= 0) {
-//         return base;
-//     }
-
-//     for (let i = 1; i <= repeat; i++) {
-//         str += `${base * i}---`;
-//     }
-
-//     return str.slice(0, -3);
-// }
-
-// console.log(getMathResult(2,10));
-
-
-// function calculateVolumeAndArea (n) {
-//     let volume = (n * n * n)
-//     let S = (n * 6) * n;
-
-//     let str = '';
-//     return [S , volume];
-// }
-// console.log(calculateVolumeAndArea(15));
-
-function getCoupeNumber (n) {
-    if (n < 0 || typeof n != 'number' || !Number.isInteger(n)) {
-        return "Ошибка. Проверьте правильность введенного номера места"
-    } else if (n  > 36 || n == 0) {
-        return "Таких мест в вагоне не существует";
-    }
-
-    return Math.ceil(n / 4)
+    setTimeout(function () {
+        console.log(1);
+    },500);
 }
 
-console.log(getCoupeNumber(5));
+function second () {
+    console.log(2);
+}
+
+first();
+second();
+
+function learnJS (lang,callback) {
+    console.log(`Я учу: ${lang}`);
+    callback();
+}
+
+learnJS('JAVASCRIPT', done)
 
 
-
-
+function done() {
+    console.log('Я прошел данный урок');
+}

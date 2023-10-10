3db677aab7e1719f6f0bb4e91be7b49fff34b554
Mark <example@gmail.com>
2023-10-10 12:58:56 -0600

    tasks 11 , work with arrays

--- js/script.js
+++ js/script.js
@@ -1,47 +1,28 @@
 'use strict';
 
-const personalPlanPeter = {
-    name: "Peter",
-    age: "29",
-    skills: {
-        languages: ['ru', 'eng'],
-        programmingLangs: {
-            js: '20%',
-            php: '10%'
-        },
-        exp: '1 month'
-    },
-    showAgeAndLangs: function (obj) {
-        let str = `Мне ${obj.age} и я владею языками: `
+const family = ['Peter', 'Ann', 'Alex', 'Linda'];
 
-        for (let key of obj.skills.languages) {
-            str += `${key} `.toUpperCase();
-        }
-
-        return str.trim(); // метод трим убрал пробелы вконце
-    }
-}
-console.log(personalPlanPeter.showAgeAndLangs(personalPlanPeter));
-
-function showExperience (obj) {
-    const {exp} = obj.skills
-
-    return exp;
-}
-
-// console.log(showExperience(personalPlanPeter));
-
-function showProgrammingLangs (obj) {
-    
-    let str = '';
-
-    for (let key in obj.skills.programmingLangs) {
-        str += `Язык ${key} изучен на ${obj.skills.programmingLangs[key]}\n`
+function showFamily (arr) {
+    let str =  'Семья состоит из: '
+    if (arr === '') {
+        return 'Семья пуста';
     }
 
-    return str
+    for (let key of arr) {
+        str += key + ' '
+    }
+
+    return str.trim();
 }
 
-// console.log(showProgrammingLangs(personalPlanPeter));
+// console.log(showFamily(family));
 
 
+const favoriteCities = ['liSBon', 'ROME', 'miLan', 'Dublin'];
+function standardizeStrings (arr) {
+    let str = arr.join('\n');
+
+    return str.toLowerCase();
+}
+
+console.log(standardizeStrings(favoriteCities));

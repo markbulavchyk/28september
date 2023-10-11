4d487124092b09c2726fbb9c35f0b110ff7e5d53
Mark <example@gmail.com>
2023-10-10 18:18:26 -0600

    lesson 37,practice part 4, use objects

--- js/script.js
+++ js/script.js
@@ -1,29 +1,118 @@
 'use strict';
 
+const personalMovieDB = {
+    count: 0,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+    start: function () {
+        personalMovieDB.count = prompt('Сколько фильмов вы уже посмотрели?', '');
 
+        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = prompt('Сколько фильмов вы уже посмотрели?', '');
+        }
+    },
+    rememberMyFilms: function () {
+        for (let i = 0; i < 2; i++) {
+            const a = prompt('Один из последних просмотренных фильмов?', '').trim(),
+                b = prompt('На сколько оцените его?', '');
 
-let str = 'some';
+            if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+                personalMovieDB.movies[a] = b;
+                console.log('done');
+            } else {
+                i--;
+            }
+        }
+    },
+    detectPersonalLevel: function () {
+        if (personalMovieDB.count < 10) {
+            console.log('Просмотрено довольно мало фильмов');
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+            console.log('Вы классический зритель');
+        } else if (personalMovieDB.count >= 30) {
+            console.log('Вы киноман');
+        } else {
+            console.log('Произошла ошибка');
+        }
+    },
+    showMyDB: function () {
+        if (personalMovieDB.privat == false) {
+            console.log(personalMovieDB);
+        }
+    },
+    toggleVisibleMyDB: function () {
+        if (personalMovieDB.privat) {
+            personalMovieDB.privat = false;
+        } else {
+            personalMovieDB.privat = true;
+        }
+    },
+    writeYourGenres: function () {
+        for (let i = 1; i < 4; i++) {
+            let genre = prompt(`Ваш любимый жанр под номером ${i}`, ``);
 
-let strObj = new String();
-
-
-const soldier = {
-    health: 400,
-    armor: 100,
-    sayHello: function () {
-        console.log('hello');
+            if (genre === '' || genre === null) {
+                console.log('Вы ввели некорректные данные или не ввели их вовсе');
+                i--;
+            } else {
+                personalMovieDB.genres[i - 1] = genre;
+            }
+        }
+        personalMovieDB.genres.forEach((item, i) => {
+            console.log(`Любимый жанр ${i + 1}: - это  ${item}`);
+        });
     }
 }
 
-// const john = {
-//     health: 100
+personalMovieDB.start();
+
+personalMovieDB.rememberMyFilms();
+
+personalMovieDB.detectPersonalLevel();
+
+personalMovieDB.writeYourGenres();
+
+personalMovieDB.showMyDB();
+
+// function show(hidden) {
+//     if (!hidden) {
+//         console.log('база не приватная');
+//     }
+// }
+// show(personalMovieDB.privat)
+// в аргумент передается значение privat = фолсе , если прийвет не равен тру , а по умолчанию он не равен тру то есть равен фолс то выводим msg
+
+
+
+
+// function reverse (str) {
+
+//     if (typeof(str) !== 'string') {
+//         return "Ошибка!";
+
+//     }
+
+//     const arr = str.split('').reverse().join('');
+//     // const arrReverse = arr.reverse();
+//     // const reservedStr = arrReverse.join('');
+//     // return reservedStr
+//     return arr
 // }
 
-// john.__proto__ = soldier;
+// const someString = 'This is some strange string';
 
-Object.setPrototypeOf(john, soldier);
+// // console.log(reverse(someString));
 
-const john = Object.create(soldier);
+// const baseCurrencies = ['USD', 'EUR'];
+// const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
 
 
+// function availableCurr(array, wichIsGone) {
+//     if (array.length === 0) {
+//         return 'Нет доступных валют'
+//     }
+// }
 
+// console.log(availableCurr());

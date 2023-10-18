e3c5043e211a1bc800a22047a9bc995d53d70f87
Mark <example@gmail.com>
2023-10-17 18:12:09 -0600

    lesson 38 ,debugger

--- js/script.js
+++ js/script.js
@@ -1,47 +1,22 @@
 'use strict';
 
-// function reverse (str) {
-
-//     if (typeof(str) !== 'string') {
-//         return "Ошибка!";
-
-//     }
-
-//     const arr = str.split('').reverse().join('');
-//     // const arrReverse = arr.reverse();
-//     // const reservedStr = arrReverse.join('');
-//     // return reservedStr
-//     return arr
-// }
-
-// const someString = 'This is some strange string';
-
-// console.log(reverse(someString));
-
-const baseCurrencies = ['USD', 'EUR'];
-
-const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
-
-
-function availableCurr(array, missCurr) {
-    if (array.length === 0) {
-        return 'Нет доступных валют'
-    }
-
-    let str = `Доступные валюты:\n`;
-
-    const AllCurr = [...baseCurrencies, additionalCurrencies];
-
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] === missCurr) {
-            continue;
-        }
-        str += `${array[i]}\n`
-    }
-
-
-    return str.trim();
+function hello() {
+    console.log('Hello world');
 }
 
-console.log(availableCurr(['UAH', 'RUB', 'CHY'], 'CHY'));
+hello();
 
+function hi() {
+    console.log('Say hi!');
+    debugger;
+}
+
+
+hi();
+
+const arr = [1, 14, 4, 30, 54],
+    sorted = arr.sort(compareNum);
+
+function compareNum(a, b) {
+    return a - b;
+}
